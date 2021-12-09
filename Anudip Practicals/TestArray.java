233b437d8ca60419e0f704ea22617537a8fd27ee
Abhishek F <50977985+Abhishekf71@users.noreply.github.com>
2021-12-09 11:05:00 +0530

    Add files via upload

---
+++ Anudip Practicals/TestArray.java
@@ -0,0 +1,18 @@
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		arr[0]=10;
+		arr[1]=12;
+		arr[2]=16;
+		arr[3]=19;
+		arr[4]=22;
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
